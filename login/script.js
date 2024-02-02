362c0b05dd74164f6acdce2d6924550db5c22f48
diEguSG <diegooosgarcia@gmail.com>
2024-02-01 21:21:19 -0300

    Nome da pasta home alterado, nome do arquivo .html da pasta home alterado home.html->index.html. Inicio da validação do Login do usuário e criação do token

--- login/script.js
+++ login/script.js
@@ -17,6 +17,10 @@ async function login(){
         senha: document.querySelector("#inp-senha").value 
     }
 
+    if(usuario.email == "" || usuario.senha == ""){
+        console.log("Campos em Brancos");   
+    }
+
     const usuario_json = JSON.stringify(usuario);
     
     const res = await fetch(`${baseURL}/login`,
@@ -25,4 +29,10 @@ async function login(){
         method: "POST",
         body: usuario_json
     })   
+
+    if(res.status == 200){
+        const res_json = await res.json()
+        localStorage.setItem("@token", res_json.acesso_token);
+        window.location.href = '../home/index.html';
+    }
 }
