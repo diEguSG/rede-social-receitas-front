c5334688e253747ca159434e86e35628acf3ca04
Rodrigo016 <Rodrigobeilke16@gmail.com>
2024-02-05 21:39:45 -0300

    home front

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,10 @@
-function curtir() {
+function curtir(id) {
+   
+    let likeCount = parseInt(localStorage.getItem(`likeCount${id}`)) || 0; 
+    console.log(likeCount)
     likeCount++;
-    localStorage.setItem('likeCount', likeCount);
-    document.getElementById('likeCount').innerText = likeCount;
+    localStorage.setItem(`likeCount${id}`, likeCount);
+    document.getElementById(`likeBtn${id}`).innerText = 'Curtir ' + likeCount;
 }
 async function getreceita(){
     const res = await fetch("http://localhost:3003/receita")
@@ -15,28 +18,28 @@ async function getreceita(){
             <div>
                 <img src="https://i.pinimg.com/550x/fd/b0/50/fdb050d4b24a2d0afacbf934113b0112.jpg" alt=""class="fotoperfil">
                     <h3>
-                            Hambúrger
+                         Hambúrger
                     </h3>
             </div>
                 <img src="https://s2-casavogue.glbimg.com/GRF9KCq-1hiz5uSs-xX9Go_KqIc=/0x0:2048x1365/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_d72fd4bf0af74c0c89d27a5a226dbbf8/internal_photos/bs/2022/p/X/eb4KQdToys327cGqnRGg/receita-ceboloni-bacon.jpg" alt="Receita de Hambúrger Artesanal">
-                    <div class="botao">
+                    <div class="botao" id="desc${element.id}">
                         <button><a href="https://api.whatsapp.com/send?text=[Hambúrger Caseiro]">Compartilhar</a></button>    
-                        <button id="likeBtn" onclick="curtir()">Curtir <span id="likeCount">0</span></button>                        
-                        <button class="vermais">Ver Mais</button>                              
+                        <button id="likeBtn${element.id}" onclick="curtir(${element.id})">Curtir <span id="likeCount">0</span></button>                        
+                        <button class="vermais" id="ver${element.id}">Ver Mais</button>                              
                     </div>
         </li>`)
+        const btnvermais = document.getElementById(`ver${element.id}`)
+        btnvermais.addEventListener("click",()=>{
+        btnvermais.setAttribute("style","display:none;")
+        const div = document.getElementById(`desc${element.id}`)
+        div.insertAdjacentHTML("beforeend", `  <li><p>${element.descricao}</p></li> `)
+       
     });
-    
-    let likeCount = localStorage.getItem('likeCount') || 0;
-    document.getElementById('likeCount').innerText = likeCount;
-    const btnvermais = document.querySelector(".vermais")
-    btnvermais.addEventListener("click",()=>{
-    btnvermais.setAttribute("style","display:none;")
-    const div = document.querySelector(".botao")
-    div.insertAdjacentHTML("beforeend", `<p>Ingredientes:Pão de hambúrguer,Carne moída com 20 a 25% de gordura (Sugestão: Fraldinha),Sal a gosto,Queijo prato,Cebola,Azeite,Açúcar mascavo,Maionese,Fatias de Bacon</p> `)
-
-})
+   
+   
+    })
 }
+
  getreceita()
 
 
