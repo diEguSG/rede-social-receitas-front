dcc60ce43d364a305afd341eb6dc58bd378d3463
Rodrigo016 <Rodrigobeilke16@gmail.com>
2024-02-07 21:19:48 -0300

    funcionalidade de curtir e descurtir no botão

--- Home/script.js
+++ Home/script.js
@@ -12,15 +12,20 @@ img_perfil_usuario.addEventListener('click', ()=>{
     window.location.href = '../tela_perfil/index.html';
 })
 
-
-function curtir() {
+function curtir(id) {
     let likeCount = parseInt(localStorage.getItem(`likeCount${id}`)) || 0; 
-    console.log(likeCount)
     likeCount++;
     localStorage.setItem(`likeCount${id}`, likeCount);
-    document.getElementById(`likeBtn${id}`).innerText = 'curtir ' + likeCount;
+    document.getElementById(`likeBtn${id}`).innerText = 'Curtir ' + likeCount;
 }
-
+function descurtir(id) {
+    let likeCount = parseInt(localStorage.getItem(`likeCount${id}`)) || 0; 
+    if (likeCount > 0) {
+        likeCount--;
+        localStorage.setItem(`likeCount${id}`, likeCount);
+        document.getElementById(`likeBtn${id}`).innerText = 'Curtir ' + likeCount;
+    }
+}
 async function getreceita(){
     const res = await fetch(`${baseURL}/receita`)
     const receita = await res.json()
@@ -39,16 +44,20 @@ async function getreceita(){
                 <img src="https://s2-casavogue.glbimg.com/GRF9KCq-1hiz5uSs-xX9Go_KqIc=/0x0:2048x1365/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_d72fd4bf0af74c0c89d27a5a226dbbf8/internal_photos/bs/2022/p/X/eb4KQdToys327cGqnRGg/receita-ceboloni-bacon.jpg" alt="Receita de Hambúrger Artesanal">
                     <div class="botao" id="desc${element.id}">
                         <button><a href="https://api.whatsapp.com/send?text=[Hambúrger Caseiro]">Compartilhar</a></button>    
-                        <button id="likeBtn${element.id}" onclick="curtir(${element.id})">Curtir <span id="likeCount">0</span></button>                        
+                        <button id="likeBtn${element.id}">Curtir<span id="likeCount">0</span></button>                        
                         <button class="vermais" id="ver${element.id}">Ver Mais</button>                              
                     </div>
         </li>`)
-        const btnvermais = document.getElementById(`ver${element.id}`)
-        btnvermais.addEventListener("click",()=>{
-        btnvermais.setAttribute("style","display:none;")
-        const div = document.getElementById(`desc${element.id}`)
-        div.insertAdjacentHTML("beforeend", `  <li><p>${element.descricao}</p></li> `)     
-    });  
+        const btncurtir = document.getElementById(`likeBtn${element.id}`)
+        btncurtir.addEventListener("click", () => {
+            if (btncurtir.classList.contains('curtido')) {
+                descurtir(element.id);
+                btncurtir.classList.remove('curtido');
+            } else {
+                curtir(element.id);
+                btncurtir.classList.add('curtido');
+            }
+        });
     })
 }
  getreceita()
