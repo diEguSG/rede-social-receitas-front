4474b4c9b10bb0913ee65ba170e3ebedfd97eb91
Rodrigo016 <Rodrigobeilke16@gmail.com>
2024-02-08 21:33:27 -0300

    botão curtir está adicionando e removendo

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,9 @@
 import {baseURL} from "../conexao_servidor.js"
 
 const token = localStorage.getItem("@token-usuario");
-
+const myHeaders = {
+    "Content-Type": "application/json"
+}
 if(!token){
     window.location.replace("../login/index.html");
 }
@@ -12,25 +14,35 @@ img_perfil_usuario.addEventListener('click', ()=>{
     window.location.href = '../tela_perfil/index.html';
 })
 
-function curtir(id) {
-    let likeCount = parseInt(localStorage.getItem(`likeCount${id}`)) || 0; 
-    likeCount++;
-    localStorage.setItem(`likeCount${id}`, likeCount);
-    document.getElementById(`likeBtn${id}`).innerText = 'Curtir ' + likeCount;
+async function curtir(id) {
+    localStorage.setItem(`likeCount${id}`, true);
+    const body = JSON.stringify({curtida:true})
+    await fetch(`${baseURL}/receita/${id}/curtida`,{
+        method:"PATCH",
+        body:body,
+        headers:myHeaders
+    })
+    await getreceita()
 }
-function descurtir(id) {
-    let likeCount = parseInt(localStorage.getItem(`likeCount${id}`)) || 0; 
-    if (likeCount > 0) {
-        likeCount--;
-        localStorage.setItem(`likeCount${id}`, likeCount);
-        document.getElementById(`likeBtn${id}`).innerText = 'Curtir ' + likeCount;
-    }
+async function descurtir(id) {
+    localStorage.removeItem(`likeCount${id}`);
+        const body = JSON.stringify({ curtida: false });
+
+        await fetch(`${baseURL}/receita/${id}/curtida`, {
+            method: "PATCH",
+            body: body,
+            headers: myHeaders
+        });
+        await getreceita();
+    
+    console.log("descurtir");
 }
+
 async function getreceita(){
     const res = await fetch(`${baseURL}/receita`)
     const receita = await res.json()
-    console.log(receita)
     const ul = document.querySelector("ul")
+    ul.innerHTML=""
     receita.forEach(element => {
         console.log(element)
         ul.insertAdjacentHTML("beforeend",`
@@ -44,13 +56,14 @@ async function getreceita(){
                 <img src="https://s2-casavogue.glbimg.com/GRF9KCq-1hiz5uSs-xX9Go_KqIc=/0x0:2048x1365/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_d72fd4bf0af74c0c89d27a5a226dbbf8/internal_photos/bs/2022/p/X/eb4KQdToys327cGqnRGg/receita-ceboloni-bacon.jpg" alt="Receita de Hambúrger Artesanal">
                     <div class="botao" id="desc${element.id}">
                         <button><a href="https://api.whatsapp.com/send?text=[Hambúrger Caseiro]">Compartilhar</a></button>    
-                        <button id="likeBtn${element.id}">Curtir<span id="likeCount">0</span></button>                        
+                        <button id="likeBtn${element.id}">Curtir<span id="likeCount">${element.curtida}</span></button>                        
                         <button class="vermais" id="ver${element.id}">Ver Mais</button>                              
                     </div>
         </li>`)
         const btncurtir = document.getElementById(`likeBtn${element.id}`)
         btncurtir.addEventListener("click", () => {
-            if (btncurtir.classList.contains('curtido')) {
+            const curtido = localStorage.getItem(`likeCount${element.id}`)
+            if (curtido) {
                 descurtir(element.id);
                 btncurtir.classList.remove('curtido');
             } else {
